927bc6080ab03c77f7b109f7d2ed21bf357fc92e
TinkerTavern <43844772+TinkerTavern@users.noreply.github.com>
2018-11-17 14:46:01 +0000

    Added Base Code

---
+++ Base Code.py
@@ -0,0 +1,17 @@
+# Engineers Gate Hackathon 2018
+# Tom Lancaster, Mathew Allington and James Lake
+# __Team
+
+
+import pandas as pd
+import statsmodels as sm
+import numpy as np
+import requests
+
+
+def main():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
