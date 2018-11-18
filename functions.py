8db5b0d593ee448eac15e2958c92397cf03e3994
jl3283 <45124657+jl3283@users.noreply.github.com>
2018-11-18 02:05:07 +0000

    Add files via upload

--- functions.py
+++ functions.py
@@ -7,9 +7,28 @@ def simMovAvg(data,lastAvg,n):
     else:
         return lastaverage+data[-1]-data[-1-n]
 
-def expMovAvg(lastVal,lastAvg,halfLife):
-    #a = exp(log(0.5)/halfLife)
-    a=halfLife
-    return (lastAvg+a*(lastVal-lastAvg))#(a*lastVal)+((1-a)*lastAvg)
+def getα(halfLife):
+    return exp(log(0.5)/halfLife)
 
- 
+def expMovAvg(lastVal,lastAMA,halfLife):
+    a=getα(halfLife)
+    return (lastAMA+a*(lastVal-lastAMA))#(a*lastVal)+((1-a)*lastAvg)
+
+def movStandDev(data,lastSMA,curSMA,n):
+    if len(data)<n:
+        return 0
+    elif len(data)==n:
+        avg=sum(data)/n
+        tot = 0
+        for x in range(n):
+            tot+data[x]-avg
+        return (tot/(n-1))**0.5
+    else:
+        var = ((data[-1]-data[-n-1])*(data[-1]-curSMA+data[-n-1]-lastSMA))/(n-1)
+        return var**0.5
+
+def expMovStandDev(lastVal,lastEMA,last,a):
+    return ((lastEMA+a*(lastVal-LastEMA)**2)*(1-a))**0.5
+
+
+    
